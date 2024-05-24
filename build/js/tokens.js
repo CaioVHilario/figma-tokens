669a0f1b9e1493f3292a4d150c6133ae86d23d91
CaioVHilario <caio.hilario@ufv.br>
2024-05-24 10:03:31 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,20 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 24 May 2024 11:37:24 GMT
+ */
+
+module.exports = {
+  "ColorGray9000": "#121214",
+  "ColorGray9001": "#000000",
+  "ColorGray9002": "#000000",
+  "ColorGray800": "#1c1c1f",
+  "ColorGreen700": "#14532d",
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "VariableCollectionGray200": "#c1c1c4ff",
+  "VariableCollectionGray500": "#50505cff",
+  "VariableCollectionGray800": "#1c1c1fff",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionGreen700": "#14532dff"
+};
